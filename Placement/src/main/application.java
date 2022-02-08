44a0b2d834c686c1c2384a56332ed461ccf429a2
ashwini-balu <94046261+ashwini-balu@users.noreply.github.com>
2022-02-08 09:55:24 +0530

    Create application.java

---
+++ Placement/src/main/application.java
@@ -0,0 +1,12 @@
+spring.datasource.url=jdbc:mysql://localhost:3306/batch_5723?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC
+server.error.whitelabel.enabled=false
+server.port=8380
+spring.datasource.username=root
+spring.datasource.password=root
+#logging.level.root=WARN
+spring.jpa.open-in-view=false
+spring.thymeleaf.cache=false
+
+spring.jpa.show-sql=true
+api.base.path = http://localhost:8380/adminlogin
+spring.thymeleaf.mode=HTML5
